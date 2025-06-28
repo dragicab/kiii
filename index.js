618d99859b30fd4e2fe4ff549542572f842505df
Mitko <mitkoiliev505@gmail.com>
2025-06-29 00:30:45 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const routeTasks = require('./src/routes/tasks');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 
-app.use('/api/tasks', routeTasks, (req, res) => res.sendStatus(401));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// Enable CORS for the React app
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
 });
+
+app.use('/api/tasks', routeTasks);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
--- src/db.js
+++ src/db.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/simple-mern');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongodb/simple-mern';
+
+const connectWithRetry = () => {
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch(err => {
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.');
+    setTimeout(connectWithRetry, 5000);
+  });
+};
+
+connectWithRetry();
 
 module.exports = mongoose;
